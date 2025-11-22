63e23060c67880f85647a48d8a0db630d778d93a
UrielGGarcia <227o01221@itsx.edu.mx>
2025-11-22 02:09:29 -0600

    fix: simplify main to fix render build

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as express from 'express';
-import helmet from 'helmet';
-import compression from 'compression';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Seguridad
-  app.use(helmet());
-
-  // CORS
-  app.enableCors({
-    origin: '*',
-  });
-
-
-
-  // Webhook STRIPE
-  app.use('/stripe/webhook', express.raw({ type: 'application/json' }));
-
-  // Swagger SOLO en desarrollo
-  if (process.env.NODE_ENV !== 'production') {
-    const config = new DocumentBuilder()
-      .setTitle('API de LitHub')
-      .setDescription('Documentación de la API construida con NestJS y Swagger')
-      .setVersion('1.0')
-      .build();
-
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api/docs', app, document);
-  }
+  app.enableCors({ origin: '*' });
 
   await app.listen(process.env.PORT || 3003, '0.0.0.0');
 }
